1c9ee085b095db415b2f653d16655d2ad155921d
mene0903 <cude0903@naver.com>
2025-05-17 22:07:00 +0900

    Mathematics_practice

---
+++ algorithmJava/src/Mathematics/Num_2217.java
@@ -0,0 +1,28 @@
+package Mathematics;
+
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Num_2217 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		int rope[] = new int[num];
+		int max = 0;
+		int cnt = 1;
+		
+		for(int i=0; i<num; i++) {
+			rope[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(rope);
+		for(int i=num-1; i>-1; i--) {
+			if (rope[i]*cnt > max) max = rope[i]*cnt;
+			cnt += 1;
+		}
+		System.out.println(max);
+	}
+}
