c839b16696ae1b65840026a98c3c1c96dc4f40b1
mene0903 <cude0903@naver.com>
2025-05-17 21:47:41 +0900

    Mathematics_practice(DFS algorithm)

---
+++ algorithmJava/src/Mathematics/Num_6603.java
@@ -0,0 +1,44 @@
+package Mathematics;
+
+import java.util.Scanner;
+
+public class Num_6603 {
+	static int arr[];
+    static int[] selected = new int[6];
+
+	static void dfs(int a, int b) {
+		if(a == 6) {
+			for(int i=0; i<selected.length; i++) {
+                System.out.print(selected[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=b; i<arr.length; i++) {
+			selected[a] = arr[i];
+			dfs(a+1, i+1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean tf = true;
+	
+		while(true) {
+			int num = sc.nextInt();
+			
+			if(num==0) break;
+			
+			arr = new int[num];
+			for (int i=0; i<num; i++) {
+				arr[i] = sc.nextInt();
+			}
+			if(!tf) System.out.println();
+			tf = false;
+			
+			dfs(0,0);
+		}
+	}
+
+}
